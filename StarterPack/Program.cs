ef814073eb9432d6cdc9032201f947b96f3787f0
AmritK2 <amrit.kaur@myob.com>
2017-12-12 11:57:33 +1300

    code builds but still doesn't fetch the data

--- StarterPack/Program.cs
+++ StarterPack/Program.cs
@@ -1,42 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net.Http;
-using System.Net;
-using System.IO;
-using Newtonsoft.json.Linq.Jobject;
+using Newtonsoft.Json.Linq;
 
 namespace StarterPack
 {
     public class Program
     {
-        public void User(string json)
+        private string json;
+
+        public Program(string json)
         {
             JObject jObject = JObject.Parse(json);
-            Jtoken jUser = jObject["username"];
-            name = (string) jUser["name"];
-            honor = (string) jUser["honor"];
-            clan = (string) jUser["clan"];
-            leaderboardPosition = (string) jUser["leaderboardPosition"];
+            JToken jUser = jObject["username"];
+            Name = (string)jUser["name"];
+            Honor = (string)jUser["Honor"];
+            Clan = (string)jUser["Clan"];
+            LeaderboardPosition = (string)jUser["LeaderboardPosition"];
         }
-
-        public string name { get; set; }
-        public string honor { get; set; }
-        public string clan { get; set; }
-        public string leaderboardPosition { get; set; }
+        
+        public string Name { get; set; }
+        public string Honor { get; set; }
+        public string Clan { get; set; }
+        public string LeaderboardPosition { get; set; }
 
         private void GetInfo()
         {
             string json= "https://www.codewars.com/api/v1/users/Amrit2";
             Program user = new Program(json);
 
-            Console.WriteLine("Name: " + user.name);
-            Console.WriteLine("Honor: " + user.honor);
-            Console.WriteLine("Clan: " + user.clan);
-            Console.WriteLine("Leader Board Position: " + user.leaderboardPosition);
+            Console.WriteLine("Name: " + user.Name);
+            Console.WriteLine("Honor: " + user.Honor);
+            Console.WriteLine("Clan: " + user.Clan);
+            Console.WriteLine("Leader Board Position: " + user.LeaderboardPosition);
         }
         static void Main(string[] args)
         {
