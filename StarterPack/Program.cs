70607a893b7bc497ab9c6cee5f7e94c40a9c3806
AmritK2 <amrit.kaur@myob.com>
2017-12-11 16:36:07 +1300

    nothing working yet

--- StarterPack/Program.cs
+++ StarterPack/Program.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace StarterPack
 {
     class Program
     {
+        static HttpClient client = new HttpClient();
+
+        
+        WebRequest request = WebRequest.Create("https://www.codewars.com/api/v1/users/:id_or_username");
+        WebResponse response = request.GetResponse(); 
+        Console.WriteLine (((HttpWebResponse)response).StatusDescription);  
+        
         static void Main(string[] args)
         {
         }
