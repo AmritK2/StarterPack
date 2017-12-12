94bcedd879345987e0d74ac0cca44b01c8da24d7
AmritK2 <amrit.kaur@myob.com>
2017-12-12 15:37:38 +1300

    get request code added

--- StarterPack/Program.cs
+++ StarterPack/Program.cs
@@ -1,42 +1,44 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
 namespace StarterPack
 {
     public class Program
     {
-        private string json;
-
-        public Program(string json)
-        {
-            JObject jObject = JObject.Parse(json);
-            JToken jUser = jObject["username"];
-            Name = (string)jUser["name"];
-            Honor = (string)jUser["Honor"];
-            Clan = (string)jUser["Clan"];
-            LeaderboardPosition = (string)jUser["LeaderboardPosition"];
-        }
-        
-        public string Name { get; set; }
-        public string Honor { get; set; }
-        public string Clan { get; set; }
-        public string LeaderboardPosition { get; set; }
-
-        private void GetInfo()
-        {
-            string json= "https://www.codewars.com/api/v1/users/Amrit2";
-            Program user = new Program(json);
-
-            Console.WriteLine("Name: " + user.Name);
-            Console.WriteLine("Honor: " + user.Honor);
-            Console.WriteLine("Clan: " + user.Clan);
-            Console.WriteLine("Leader Board Position: " + user.LeaderboardPosition);
-        }
         static void Main(string[] args)
         {
+            GetRequest("https://www.codewars.com/api/v1/users/Amrit2");
         }
+
+        static async void GetRequest(string url)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+            HttpContent content = response.Content;
+            string info = await content.ReadAsStringAsync();
+            Console.WriteLine(info);
+        }
+        /*
+        public static async Task<string> GetUserInfo(string url)
+        {
+            var client = new HttpClient();
+            string info =  await client.GetStringAsync(url);
+            return info;
+        }
+
+        static void Main(string[] args)
+        {
+            var response = GetUserInfo("https://www.codewars.com/api/v1/users/Amrit2");
+            Console.WriteLine(response);
+        }
+        */
     }
 }
+
+
+
 
 
 /*
@@ -56,14 +58,31 @@ namespace StarterPack
             }
             
         }
+        public Program(string json)
+        {
+            JObject jObject = JObject.Parse(json);
+            JToken jUser = jObject["username"];
+            Name = (string)jUser["name"];
+            Honor = (string)jUser["honor"];
+            Clan = (string)jUser["clan"];
+            LeaderboardPosition = (string)jUser["leaderboardPosition"];
+        }
 
-        WebRequest request = WebRequest.Create("https://www.codewars.com/api/v1/users/Amrit2");
-        WebResponse response = request.GetResponse(); 
-        Console.WriteLine (((HttpWebResponse)response).StatusDescription);    
-        Stream dataStream = response.GetResponseStream ();    
-        StreamReader reader = new StreamReader (dataStream);  
-        string responseFromServer = reader.ReadToEnd ();  
-        Console.WriteLine(responseFromServer);  
-        reader.Close();  
-        response.Close();  
-*/      
+    private void GetInfo()
+        {
+            const string url = "https://www.codewars.com/api/v1/users/Amrit2";
+            var user = new Program(url);
+
+            Console.WriteLine("Name: " + user.Name);
+            Console.WriteLine("Honor: " + user.Honor);
+            Console.WriteLine("Clan: " + user.Clan);
+            Console.WriteLine("Leader Board Position: " + user.LeaderboardPosition);
+        }
+
+    public string Name { get; set; }
+        public string Honor { get; set; }
+        public string Clan { get; set; }
+        public string LeaderboardPosition { get; set; }
+
+        
+*/
